e206f7866b60585c4ca615a91b697d45a9209592
tealvin <tealvinsjhi11ly@gmail.com>
2023-01-28 06:19:07 +0800

    render dataTodos into todoItems in TodoList

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,5 +1,13 @@
-const TodoList = () => {
-  return <div>Todo List</div>;
+const TodoList = ({ todos: dataTodos = [] } = {}) => {
+  const todoItems =
+    dataTodos.length &&
+    dataTodos.map((dataTodo = {}) => {
+      const { id = "", todo = "" } = dataTodo;
+
+      return <li key={id}>{todo}</li>;
+    });
+
+  return <ul>{todoItems}</ul>;
 };
 
 export default TodoList;
