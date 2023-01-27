7ed0c3aca6c775e9024dd7101dc0fc8a340307c1
tealvin <tealvinsjhi11ly@gmail.com>
2023-01-28 05:22:35 +0800

    re-write App to a class component and mock fetching todos data in it

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
+import { Component } from "react";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
-function App() {
-  return (
-    <div>
-      <h1>Todo App</h1>
-      <TodoInput />
-      <TodoList />
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [],
+  };
+
+  componentDidMount() {
+    const dataTodos = [
+      {
+        id: "1",
+        todo: "wash dishes",
+      },
+      {
+        id: "2",
+        todo: "buy blue berries",
+      },
+      {
+        id: "3",
+        todo: "buy flight tickets",
+      },
+    ];
+    this.setState({
+      todos: dataTodos,
+    });
+  }
+
+  render() {
+    const { todos = [] } = this.state;
+
+    return (
+      <div>
+        <h1>Todo App</h1>
+        <TodoInput />
+        <TodoList todos={todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
