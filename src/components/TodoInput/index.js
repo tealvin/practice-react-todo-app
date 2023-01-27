b56bfcc9d46362f0c8557279332d5c6d93124a9a
tealvin <tealvinsjhi11ly@gmail.com>
2023-01-28 06:40:40 +0800

    Re-write TodoInput to a class component and complete handleInputChange and handleAddTodo

--- src/components/TodoInput/index.js
+++ src/components/TodoInput/index.js
@@ -1,5 +1,33 @@
-const TodoInput = () => {
-  return <div>Todo Input</div>;
-};
+import { Component } from "react";
+class TodoInput extends Component {
+  state = { inputValue: "" };
+
+  handleInputChange = (e = {}) => {
+    const { target: { value: userInput = "" } = {} } = e;
+    this.setState({ inputValue: userInput });
+  };
+
+  handleAddTodo = () => {
+    const { inputValue = "" } = this.state;
+    const { addTodo = () => {} } = this.props;
+    addTodo(inputValue);
+    this.setState({ inputValue: "" });
+  };
+
+  render() {
+    const { inputValue = "" } = this.state;
+
+    return (
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={this.handleInputChange}
+        />
+        <button onClick={this.handleAddTodo}>Add</button>
+      </div>
+    );
+  }
+}
 
 export default TodoInput;
