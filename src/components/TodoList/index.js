7e3082f3badf27e72e189690b1f910a29c1e8129
tealvin <tealvinsjhi11ly@gmail.com>
2023-01-28 08:48:39 +0800

    extract TodoItem

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,10 +1,23 @@
-const TodoList = ({ todos: dataTodos = [] } = {}) => {
+const TodoItem = ({ id = "", todo = "", deleteTodo = () => {} } = {}) => {
+  const handleDeleteTodo = () => {
+    deleteTodo(id);
+  };
+
+  return (
+    <li>
+      <button onClick={handleDeleteTodo}>delete</button>
+      <span>{todo}</span>
+    </li>
+  );
+};
+
+const TodoList = ({ todos: dataTodos = [], deleteTodo = () => {} } = {}) => {
   const todoItems =
     dataTodos.length &&
     dataTodos.map((dataTodo = {}) => {
       const { id = "", todo = "" } = dataTodo;
 
-      return <li key={id}>{todo}</li>;
+      return <TodoItem key={id} id={id} todo={todo} deleteTodo={deleteTodo} />;
     });
 
   return <ul>{todoItems}</ul>;
